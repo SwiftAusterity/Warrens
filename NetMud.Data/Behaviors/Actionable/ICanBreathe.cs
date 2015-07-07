a1393ec242d00fd570a69d785bd7c3c30d3c3ab3
dnissenfeld <dnissenfeld@swankmp.com>
2015-07-07 14:23:57 -0500

    More behavioral patterns

---
+++ NetMud.Data/Behaviors/Actionable/ICanBreathe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMud.Data.Base.Supporting;
+
+namespace NetMud.Data.Behaviors.Actionable
+{
+    interface ICanBreathe<IMaterial>
+    {
+    }
+}
---
+++ NetMud.Data/Behaviors/Actionable/ICanConsume.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMud.Data.Behaviors.Actionable
+{
+    interface ICanConsume
+    {
+    }
+}
