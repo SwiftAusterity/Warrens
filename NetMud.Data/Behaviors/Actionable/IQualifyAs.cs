36fe5fa27538260f5765e1026bc28cf97048079a
dnissenfeld <dnissenfeld@swankmp.com>
2015-07-07 14:41:22 -0500

    More patterns

---
+++ NetMud.Data/Behaviors/Actionable/IQualifyAs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMud.Data.Behaviors.Automation;
+
+namespace NetMud.Data.Behaviors.Actionable
+{
+    interface IQualifyAs<ICraftingType>
+    {
+    }
+}
---
+++ NetMud.Data/Behaviors/Automation/IEat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMud.Data.Behaviors.Automation
+{
+    interface IEat<DietType>
+    {
+    }
+
+    public enum DietType
+    {
+        Herbivore,
+        Carnivore,
+        Necrovore,
+        Metalvore,
+        Magiviore,
+        Spirivore
+    }
+}
