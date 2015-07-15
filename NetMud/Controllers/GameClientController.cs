f3580a7608a595478e20d438219db78bcd1c13a7
dnissenfeld <dnissenfeld@swankmp.com>
2015-07-15 09:39:15 -0500

    i know who is logged in now

--- NetMud/Controllers/GameClientController.cs
+++ NetMud/Controllers/GameClientController.cs
@@ -1,13 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NetMud.Models;
+using System.Web.Mvc;
 
 namespace Controllers
 {
     public class GameClientController : Controller
     {
+        /// <summary>
+        /// Application DB context
+        /// </summary>
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// User manager - attached to application DB context
+        /// </summary>
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
         // GET: GameClient
         public ActionResult Index()
         {
-            return View();
+            var model = new GameContextModel();
+            model.authedUser = UserManager.FindById(User.Identity.GetUserId());
+
+            return View(model);
+        }
+
+        private void GetCurrentUserContext()
+        {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.ApplicationDbContext));
         }
     }
 }
