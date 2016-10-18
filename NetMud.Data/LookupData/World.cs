161b970da33148a32e8ad6cc9141f082256cfaf3
SwiftAusterity <swiftausterity@gmail.com>
2016-10-18 03:59:34 -0500

    rrrrr

--- NetMud.Data/LookupData/World.cs
+++ NetMud.Data/LookupData/World.cs
@@ -2,10 +2,6 @@
 using NetMud.DataStructure.Base.Place;
 using NetMud.DataStructure.SupportingClasses;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetMud.Data.LookupData
 {
