b23e0a569ca4f1bdd51c15cea74d37f97d031a8c
Daniel Nissenfeld <swiftausterity@gmail.com>
2016-11-03 15:21:04 -0500

    backing data dropdown partial

---
+++ NetMud/Models/Admin/BackingDataDropdownModel.cs
@@ -0,0 +1,24 @@
+﻿using NetMud.DataStructure.Base.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetMud.Models.Admin
+{
+    public class BackingDataDropdownModel
+    {
+        public BackingDataDropdownModel(string controlName, string label, IEnumerable<IEntityBackingData> validItemList, long selectedItemId)
+        {
+            ControlName = controlName;
+            Label = label;
+            ValidList = validItemList;
+            SelectedItemId = selectedItemId;
+        }
+
+        public string ControlName { get; set; }
+        public string Label { get; set; }
+        public IEnumerable<IEntityBackingData> ValidList { get; set; }
+        public long SelectedItemId { get; set; }
+    }
+}
