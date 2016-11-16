95ee962615b6e1e19716e536f7ff2884e9d5ff46
Daniel Nissenfeld <swiftausterity@gmail.com>
2016-11-16 11:15:53 -0600

    baseline for all nat resource admins

---
+++ NetMud/Controllers/GameAdmin/FloraController.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using NetMud.Authentication;
+using NetMud.Data.LookupData;
+using NetMud.DataAccess;
+using NetMud.DataAccess.Cache;
+using NetMud.DataStructure.Base.Entity;
+using NetMud.DataStructure.Base.EntityBackingData;
+using NetMud.DataStructure.Base.Supporting;
+using NetMud.DataStructure.Base.System;
+using NetMud.Models.Admin;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetMud.Controllers.GameAdmin
+{
+    public class FloraController : Controller
+    {
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public FloraController()
+        {
+        }
+
+        public FloraController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ActionResult Index(string SearchTerms = "", int CurrentPageNumber = 1, int ItemsPerPage = 20)
+        {
+            var vModel = new ManageFloraViewModel(BackingDataCache.GetAll<IFlora>());
+            vModel.authedUser = UserManager.FindById(User.Identity.GetUserId());
+
+            vModel.CurrentPageNumber = CurrentPageNumber;
+            vModel.ItemsPerPage = ItemsPerPage;
+            vModel.SearchTerms = SearchTerms;
+
+            return View("~/Views/GameAdmin/Flora/Index.cshtml", vModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Remove(long ID, string authorize)
+        {
+            string message = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(authorize) || !ID.ToString().Equals(authorize))
+                message = "You must check the proper authorize radio button first.";
+            else
+            {
+                var authedUser = UserManager.FindById(User.Identity.GetUserId());
+
+                var obj = BackingDataCache.Get<IFlora>(ID);
+
+                if (obj == null)
+                    message = "That does not exist";
+                else if (obj.Remove())
+                {
+                    LoggingUtility.LogAdminCommandUsage("*WEB* - RemoveFlora[" + ID.ToString() + "]", authedUser.GameAccount.GlobalIdentityHandle);
+                    message = "Delete Successful.";
+                }
+                else
+                    message = "Error; Removal failed.";
+            }
+
+            return RedirectToAction("Index", new { Message = message });
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            var vModel = new AddEditFloraViewModel();
+            vModel.authedUser = UserManager.FindById(User.Identity.GetUserId());
+            vModel.ValidMaterials = BackingDataCache.GetAll<IMaterial>();
+            vModel.ValidInanimateDatas = BackingDataCache.GetAll<IInanimateData>();
+
+            return View("~/Views/GameAdmin/Flora/Add.cshtml", vModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(AddEditFloraViewModel vModel)
+        {
+            string message = string.Empty;
+            var authedUser = UserManager.FindById(User.Identity.GetUserId());
+
+            var newObj = new Flora();
+            newObj.Name = vModel.Name;
+            newObj.HelpText = vModel.HelpText;
+            newObj.AmountMultiplier = vModel.AmountMultiplier;
+            newObj.Rarity = vModel.Rarity;
+            newObj.PuissanceVariance = vModel.PuissanceVariance;
+            newObj.ElevationRange = new Tuple<int, int>(vModel.ElevationRangeLow, vModel.ElevationRangeHigh);
+            newObj.TemperatureRange = new Tuple<int, int>(vModel.TemperatureRangeLow, vModel.TemperatureRangeHigh);
+            newObj.HumidityRange = new Tuple<int, int>(vModel.HumidityRangeLow, vModel.HumidityRangeHigh);
+
+            var newMaterials = new List<IMaterial>();
+            if (vModel.OccursIn != null)
+            {
+                foreach (var materialId in vModel.OccursIn)
+                {
+                    if (materialId >= 0)
+                    {
+                        var material = BackingDataCache.Get<IMaterial>(materialId);
+
+                        if (material != null)
+                            newMaterials.Add(material);
+                    }
+                }
+
+                if (newMaterials.Count > 0)
+                    newObj.OccursIn = newMaterials;
+            }
+
+            if (newObj.Create() == null)
+                message = "Error; Creation failed.";
+            else
+            {
+                LoggingUtility.LogAdminCommandUsage("*WEB* - AddFlora[" + newObj.ID.ToString() + "]", authedUser.GameAccount.GlobalIdentityHandle);
+                message = "Creation Successful.";
+            }
+
+            return RedirectToAction("Index", new { Message = message });
+        }
+
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+            string message = string.Empty;
+            var vModel = new AddEditFloraViewModel();
+            vModel.authedUser = UserManager.FindById(User.Identity.GetUserId());
+            vModel.ValidMaterials = BackingDataCache.GetAll<IMaterial>();
+            vModel.ValidInanimateDatas = BackingDataCache.GetAll<IInanimateData>();
+
+            var obj = BackingDataCache.Get<IFlora>(id);
+
+            if (obj == null)
+            {
+                message = "That does not exist";
+                return RedirectToAction("Index", new { Message = message });
+            }
+
+            vModel.DataObject = obj;
+            vModel.Name = obj.Name;
+            vModel.HelpText = obj.HelpText;
+            vModel.AmountMultiplier = obj.AmountMultiplier;
+            vModel.Rarity = obj.Rarity;
+            vModel.PuissanceVariance = obj.PuissanceVariance;
+            vModel.ElevationRangeHigh = obj.ElevationRange.Item2;
+            vModel.ElevationRangeLow = obj.ElevationRange.Item1;
+            vModel.TemperatureRangeHigh = obj.TemperatureRange.Item2;
+            vModel.TemperatureRangeLow = obj.TemperatureRange.Item1;
+            vModel.HumidityRangeHigh = obj.HumidityRange.Item2;
+            vModel.HumidityRangeLow = obj.HumidityRange.Item1;
+
+            return View("~/Views/GameAdmin/Flora/Edit.cshtml", vModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(long id, AddEditFloraViewModel vModel)
+        {
+            string message = string.Empty;
+            var authedUser = UserManager.FindById(User.Identity.GetUserId());
+
+            var obj = BackingDataCache.Get<IFlora>(id);
+            if (obj == null)
+            {
+                message = "That does not exist";
+                return RedirectToAction("Index", new { Message = message });
+            }
+
+            obj.Name = vModel.Name;
+            obj.HelpText = vModel.HelpText;
+            obj.AmountMultiplier = vModel.AmountMultiplier;
+            obj.Rarity = vModel.Rarity;
+            obj.PuissanceVariance = vModel.PuissanceVariance;
+            obj.ElevationRange = new Tuple<int, int>(vModel.ElevationRangeLow, vModel.ElevationRangeHigh);
+            obj.TemperatureRange = new Tuple<int, int>(vModel.TemperatureRangeLow, vModel.TemperatureRangeHigh);
+            obj.HumidityRange = new Tuple<int, int>(vModel.HumidityRangeLow, vModel.HumidityRangeHigh);
+
+            var newMaterials = new List<IMaterial>();
+            if (vModel.OccursIn != null)
+            {
+                foreach (var materialId in vModel.OccursIn)
+                {
+                    if (materialId >= 0)
+                    {
+                        var material = BackingDataCache.Get<IMaterial>(materialId);
+
+                        if (material != null)
+                            newMaterials.Add(material);
+                    }
+                }
+            }
+
+            //Might need to be blanked out
+            obj.OccursIn = newMaterials;
+
+            if (obj.Save())
+            {
+                LoggingUtility.LogAdminCommandUsage("*WEB* - EditFlora[" + obj.ID.ToString() + "]", authedUser.GameAccount.GlobalIdentityHandle);
+                message = "Edit Successful.";
+            }
+            else
+                message = "Error; Edit failed.";
+
+            return RedirectToAction("Index", new { Message = message });
+        }
+    }
+}
---
+++ NetMud/Models/Admin/MineralsViewModels.cs
@@ -0,0 +1,105 @@
+﻿using NetMud.Authentication;
+using NetMud.DataStructure.Base.EntityBackingData;
+using NetMud.DataStructure.Base.Supporting;
+using NetMud.DataStructure.Base.System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+
+namespace NetMud.Models.Admin
+{
+    public class ManageMineralsViewModel : PagedDataModel<IMineral>, BaseViewModel
+    {
+        public ApplicationUser authedUser { get; set; }
+
+        public ManageMineralsViewModel(IEnumerable<IMineral> items)
+            : base(items)
+        {
+            CurrentPageNumber = 1;
+            ItemsPerPage = 20;
+        }
+
+        internal override Func<IMineral, bool> SearchFilter
+        {
+            get
+            {
+                return item => item.Name.ToLower().Contains(SearchTerms.ToLower());
+            }
+        }
+    }
+
+    public class AddEditMineralsViewModel : BaseViewModel
+    {
+        public ApplicationUser authedUser { get; set; }
+
+        public AddEditMineralsViewModel()
+        {
+            ValidInanimateDatas = Enumerable.Empty<IInanimateData>();
+            ValidMaterials = Enumerable.Empty<IMaterial>();
+        }
+
+        [StringLength(200, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Name")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [StringLength(2000, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 20)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Help Text")]
+        public string HelpText { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Spawn Multiplier")]
+        [DataType(DataType.Text)]
+        public int AmountMultiplier { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Spawn Rarity")]
+        [DataType(DataType.Text)]
+        public int Rarity { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Puissance Variance")]
+        [DataType(DataType.Text)]
+        public int PuissanceVariance { get; set; }
+
+        [Range(-1000, 1000, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Y-Axis High")]
+        [DataType(DataType.Text)]
+        public int ElevationRangeHigh { get; set; }
+
+        [Range(-2000, 2000, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Y-Axis Low")]
+        [DataType(DataType.Text)]
+        public int ElevationRangeLow { get; set; }
+
+        [Range(-2000, 2000, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Temperature High")]
+        [DataType(DataType.Text)]
+        public int TemperatureRangeHigh { get; set; }
+
+        [Range(-2000, 2000, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Temperature Low")]
+        [DataType(DataType.Text)]
+        public int TemperatureRangeLow { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Humidity High")]
+        [DataType(DataType.Text)]
+        public int HumidityRangeHigh { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The {0} must be between {2} and {1}.")]
+        [Display(Name = "Humidity Low")]
+        [DataType(DataType.Text)]
+        public int HumidityRangeLow { get; set; }
+
+        [Display(Name = "Occurs in Material")]
+        public long[] OccursIn { get; set; }
+
+        public IEnumerable<IInanimateData> ValidInanimateDatas { get; set; }
+        public IEnumerable<IMaterial> ValidMaterials { get; set; }
+        public IMineral DataObject { get; set; }
+    }
+}
