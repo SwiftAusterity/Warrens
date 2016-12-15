d596d4427e892d614341311cdeffc9dfe2bcb4e0
Daniel Nissenfeld <swiftausterity@gmail.com>
2016-12-15 14:15:53 -0600

    still broken, just filling in "ailments"

--- NetMud.DataStructure/Base/Supporting/IAffect.cs
+++ NetMud.DataStructure/Base/Supporting/IAffect.cs
@@ -1,4 +1,5 @@
-﻿using NetMud.DataStructure.Behaviors.Actionable;
+﻿using NetMud.DataStructure.Base.System;
+using NetMud.DataStructure.Behaviors.Actionable;
 using NetMud.DataStructure.Behaviors.Automation;
 using System;
 using System.Collections.Generic;
@@ -29,63 +30,8 @@ namespace NetMud.DataStructure.Base.Supporting
         int Duration { get; set; }
 
         /// <summary>
-        /// The dispel type of the affect
+        /// The dispel resistance of the affect, -1 = undispellable
         /// </summary>
-        AffectType Type { get; set; }
-
-        /// <summary>
-        /// Chance of spread
-        /// </summary>
-        Dictionary<ContagionVector, int> AfflictionChances { get; set; }
-
-        /// <summary>
-        /// Attempt to spread this to someone else
-        /// </summary>
-        /// <param name="affected">the afflcited</param>
-        /// <param name="victim">the victim</param>
-        /// <param name="vector">How this is being spread</param>
-        /// <returns>success or failure</returns>
-        bool Afflict(IHasAffects source, ICanBeAffected victim, ContagionVector vector);
-    }
-
-    /// <summary>
-    /// Type of affect for dispelling purposes
-    /// </summary>
-    public enum AffectType
-    {
-        /// <summary>
-        /// Can be dispelling magically
-        /// </summary>
-        Magical,
-
-        /// <summary>
-        /// Undispelable by magical means, medically treatable
-        /// </summary>
-        Physical,
-
-        /// <summary>
-        /// Is a pathogenic condition
-        /// </summary>
-        Disease,
-
-        /// <summary>
-        /// Is a venom
-        /// </summary>
-        Poison,
-
-        /// <summary>
-        /// Undispellable by any means period
-        /// </summary>
-        Pure
-    }
-
-    /// <summary>
-    /// Vectors by which this contagion can be spread
-    /// </summary>
-    public enum ContagionVector
-    {
-        Pneumatic,
-        DirectContact,
-        IntimateContact
+        int DispelResistance { get; set; }
     }
 }
