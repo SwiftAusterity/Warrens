a16817dac5ff8473a63cbe8c406393bf8cd655b5
SwiftAusterity <swiftausterity@gmail.com>
2017-03-15 22:44:55 -0500

    fixing some stuff thats actually fixed locally elsewhere >.>

--- NetMud.Data/LookupData/Stratum.cs
+++ NetMud.Data/LookupData/Stratum.cs
@@ -1,8 +1,11 @@
 ﻿using NetMud.Data.System;
+using NetMud.DataAccess.Cache;
 using NetMud.DataStructure.Base.Place;
 using NetMud.DataStructure.Base.Supporting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Web.Script.Serialization;
 
 namespace NetMud.Data.LookupData
 {
@@ -28,6 +31,11 @@ namespace NetMud.Data.LookupData
         /// How humid it is in this stratum generally
         /// </summary>
         public Tuple<int, int> AmbientHumidityRange { get; set; }
+
+        public Stratum()
+        {
+            Layers = new Dictionary<string, IStratumLayer>();
+        }
     }
 
     /// <summary>
@@ -36,10 +44,26 @@ namespace NetMud.Data.LookupData
     [Serializable]
     public class StratumLayer : IStratumLayer
     {
+
+        [JsonProperty("BaseMaterial")]
+        private long _baseMaterial { get; set; }
+
         /// <summary>
         /// Material for this layer, can be null for Air layer
         /// </summary>
-        public IMaterial BaseMaterial { get; set; }
+        [JsonIgnore]
+        [ScriptIgnore]
+        public IMaterial BaseMaterial
+        {
+            get
+            {
+                return BackingDataCache.Get<IMaterial>(_baseMaterial);
+            }
+            set
+            {
+                _baseMaterial = value.ID;
+            }
+        }
 
         /// <summary>
         /// Lower Z bound for this layer
