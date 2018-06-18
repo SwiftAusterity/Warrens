465212fd7f139803a99b608f5fc0f1e3e87be3d2
Daniel Nissenfeld <swiftausterity@gmail.com>
2018-06-18 11:26:38 -0500

    tooltips!

--- NetMud/App_Start/BundleConfig.cs
+++ NetMud/App_Start/BundleConfig.cs
@@ -20,10 +20,12 @@ namespace NetMud
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/js-cookie.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.lumen.css",
+                      "~/Content/popper.css",
                       "~/Content/Site.css"));
         }
     }
