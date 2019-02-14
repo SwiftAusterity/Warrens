f8d5cc190bfe139387cf040f76584fefbc506a4a
SwiftAusterity <root@nissenfeld.com>
2019-02-14 16:17:19 -0600

    small fix

--- NetMud.DataStructure/Architectural/EntityBase/IRenderInLocation.cs
+++ NetMud.DataStructure/Architectural/EntityBase/IRenderInLocation.cs
@@ -15,7 +15,7 @@ namespace NetMud.DataStructure.Architectural.EntityBase
         /// <param name="viewer">entity initiating the command</param>
         /// <param name="sensoryTypes">What senses to include. </param>
         /// <returns>the output</returns>
-        ISensoryEvent RenderAsContents(IEntity viewer, MessagingType[] sensoryTypes);
+        IMessageCluster RenderAsContents(IEntity viewer, MessagingType[] sensoryTypes);
 
         /// <summary>
         /// A fully described short description (includes adjectives)
@@ -23,7 +23,7 @@ namespace NetMud.DataStructure.Architectural.EntityBase
         /// <param name="viewer">entity initiating the command</param>
         /// <param name="sensoryTypes">What senses to include. EMPTY/NULL = ALL</param>
         /// <returns>the output</returns>
-        ISensoryEvent GetFullDescription(IEntity viewer, MessagingType[] sensoryTypes);
+        IMessageCluster GetFullDescription(IEntity viewer, MessagingType[] sensoryTypes);
 
         /// <summary>
         /// A fully described short description (includes adjectives)
@@ -31,7 +31,7 @@ namespace NetMud.DataStructure.Architectural.EntityBase
         /// <param name="viewer">entity initiating the command</param>
         /// <param name="sensoryTypes">What senses to include. EMPTY/NULL = ALL</param>
         /// <returns>the output</returns>
-        ISensoryEvent GetImmediateDescription(IEntity viewer, MessagingType sensoryType);
+        IMessage GetImmediateDescription(IEntity viewer, MessagingType sensoryType);
 
         /// <summary>
         /// The name of a thing based on visual description
