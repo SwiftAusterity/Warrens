e1cdc04be19885805820293b9999cd516c7c3827
Danny Nissenfeld <SwiftAusterity@users.noreply.github.com>
2019-07-22 09:29:50 -0500

    Update Dictata.cs

--- NetMud.Data/Linguistic/Dictata.cs
+++ NetMud.Data/Linguistic/Dictata.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 
 namespace NetMud.Data.Linguistic
